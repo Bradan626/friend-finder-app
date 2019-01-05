a8379d55b5a912035c3d2ac87de99df233014d4f
Bradan626 <bradan626@gmail.com>
2019-01-05 00:08:16 -0500

    super broken sad from death

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Katy",
+		photo: "",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 5, 1]
+	},
+	{
+		name: "Pastor Glen",
+		photo: "",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Wayne",
+		photo: "",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Riley",
+		photo: "",
+		scores: [5, 1, 2, 1, 2, 5, 2, 1, 1, 1]
+	},
+	{
+		name: "Stewart",
+		photo: "",
+		scores: [1, 1, 5, 1, 5, 1, 3, 1, 1, 1]
+	}
+];
+
+module.exports = friends;
